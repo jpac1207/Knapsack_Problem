711e474f14b15521a878a73a62eeef6dc205bfc6
João Pedro <jpac1207@gmail.com>
2018-07-22 11:25:06 -0300

    Refatoring the code

---
+++ Knapsack Problem/Dynamic.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knapsack_Problem
+{
+    class Dynamic
+    {
+        static double[][] table;
+
+        public static void InitTable(int n, int c)
+        {
+            table = new double[n + 1][];
+
+            for (int i = 0; i < n + 1; i++)
+                table[i] = new double[c + 1];
+        }
+
+        public static void InitTableLcs(char[] x, char[] y)
+        {
+            table = new double[x.Length][];
+            printtable = new char[x.Length][];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                table[i] = new double[y.Length];
+                printtable[i] = new char[y.Length];
+            }
+        }
+
+        public static List<int> Knapsack_Problem(int[] pesos, double[] valores, int n, int c)
+        {
+            int b = 0;
+
+            for (b = 0; b <= c; b++)
+            {
+                table[0][b] = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    double a = table[i - 1][b];
+                    double candidato = 0;
+                    //Verifico a constraint para cada subproblema
+                    if (pesos[i] > b)
+                        candidato = 0;
+                    else
+                        candidato = table[i - 1][b - pesos[i]] + valores[i];
+
+                    table[i][b] = (a > candidato) ? a : candidato;
+                }
+            }
+
+            b = c;
+
+            List<int> itens = new List<int>();
+
+            for (int i = n; i >= 1; i--)
+            {
+                if (table[i][b] != table[i - 1][b])
+                {
+                    itens.Add(i);
+                    b = b - pesos[i];
+                }
+            }
+
+            return itens;
+        }
+
+        static char[][] printtable;
+
+        public static void Lcs(char[] x, char[] y)
+        {
+            int i = 0;
+
+            for (i = 1; i <= x.Length - 1; i++)
+            {
+                //table[0][b] = 0;
+
+                for (int j = 1; j <= y.Length - 1; j++)
+                {
+                    if (x[i] == y[j])
+                    {
+                        table[i][j] = table[i - 1][j - 1] + 1;
+                        printtable[i][j] = '\\';
+                    }
+                    else if (table[i - 1][j] >= table[i][j - 1])
+                    {
+                        table[i][j] = table[i - 1][j];
+                        printtable[i][j] = '|';
+                    }
+                    else
+                    {
+                        table[i][j] = table[i][j - 1];
+                        printtable[i][j] = '-';
+                    }
+
+                }
+            }
+
+            i = y.Length - 1;
+            PrintLcs(x, y, x.Length - 1, y.Length - 1);
+        }
+
+        private static void PrintLcs(char[] x, char[] y, int i, int j)
+        {
+            if (i == 0 || j == 0)
+            {
+                return;
+            }
+            if (printtable[i][j] == '\\')
+            {
+                PrintLcs(x, y, i - 1, j - 1);
+                Console.Write(x[i] + " ");
+            }
+            else if (printtable[i][j] == '|')
+            {
+                PrintLcs(x, y, i - 1, j);
+            }
+            else
+            {
+                PrintLcs(x, y, i, j - 1);
+            }
+        }
+    }
+}
--- Knapsack Problem/Program.cs
+++ Knapsack Problem/Program.cs
@@ -8,72 +8,58 @@ namespace Knapsack_Problem
 {
     class Program
     {
-        static int[][] table;
-
         static void Main(string[] args)
         {
+            KnapsackTest();
+        }
+
+        static void KnapsackTest()
+        {
+
             //int n = 4;
             //int c = 60;
             //int[] pesos = { -1, 20, 20, 30, 30 };
-            //int[] valores = { -1, 20, 30, 20, 40 };            
+            //int[] valores = { -1, 20, 30, 20, 40 };
 
             //int n = 4;
             //int c = 50;
             //int[] pesos = { -1, 40, 30, 20, 10 };
             //int[] valores = { -1, 840, 600, 400, 100 };
 
-            int n = 3;
-            int c = 4;
-            int[] pesos = { -1, 3, 2, 2 };
-            int[] valores = { -1, 5, 4, 2 };
+            //int n = 3;
+            //int c = 4;
+            //int[] pesos = { -1, 3, 2, 2 };
+            //int[] valores = { -1, 5, 4, 2 };
 
-            table = new int[n + 1][];
+            int n = 6;
+            int c = 12;
+            int[] pesos = { -1, 9, 12, 3, 4, 8, 2 };
+            double[] valores = { -1, 8, 7, 13, 11.5, 10, 10 };
 
-            for (int i = 0; i < n + 1; i++)
-                table[i] = new int[c + 1];
+            Dynamic.InitTable(n, c);
 
-            List<int> results = Knapsack_Problem(pesos, valores, n, c);
+            List<int> results = Dynamic.Knapsack_Problem(pesos, valores, n, c);
 
             results.ForEach(x => Console.Write(x + ", "));
             Console.ReadKey();
         }
 
-        static List<int> Knapsack_Problem(int[] pesos, int[] valores, int n, int c)
+        static void LcsTest()
         {
-            int b = 0;
+            //char[] x = { ' ', 'a', 'b', 'c', 'd', 'g', 'h' };
+            //char[] y = { ' ', 'a', 'e', 'd', 'f', 'h', 'r' };
 
-            for (b = 0; b <= c; b++)
-            {
-                table[0][b] = 0;
+            //char[] x = { ' ', '1', '0', '0', '1', '0', '1', '0', '1' };
+            //char[] y = { ' ', '0', '1', '0', '1', '1', '0', '1', '1', '0' };
 
-                for (int i = 1; i <= n; i++)
-                {
-                    int a = table[i - 1][b];
-                    int candidato = 0;
-                    //Verifico a constraint para cada subproblema
-                    if (pesos[i] > b)
-                        candidato = 0;
-                    else
-                        candidato = table[i - 1][b - pesos[i]] + valores[i];
+            char[] x = { ' ', '1', '2', '3', '2', '1', '2', '3', '1' };
+            char[] y = { ' ', '1', '2', '2', '3', '1', '2', '2', '1', '1' };
 
-                    table[i][b] = (a > candidato) ? a : candidato;
-                }
-            }
+            Dynamic.InitTableLcs(x, y);
+            Dynamic.Lcs(x, y);
 
-            b = c;
-
-            List<int> itens = new List<int>();
-
-            for (int i = n; i >= 1; i--)
-            {
-                if (table[i][b] != table[i - 1][b])
-                {
-                    itens.Add(i);
-                    b = b - pesos[i];
-                }
-            }
-
-            return itens;
+            Console.ReadKey();
         }
+
     }
 }
